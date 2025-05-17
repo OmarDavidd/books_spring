1f808768433c5d0a9b5cccba659569f299611e87
OmarDavidd <omarcorreonova@gmail.com>
2025-05-16 18:32:20 -0600

    cors

--- src/main/java/davo/demo_libros/DemoLibrosApplication.java
+++ src/main/java/davo/demo_libros/DemoLibrosApplication.java
@@ -2,12 +2,30 @@ package davo.demo_libros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoLibrosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoLibrosApplication.class, args);
+	}
+
+	@Configuration
+	public static class MyConfiguretion{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
